ce1c9a11331aedf9ce65811ad4361812159a1e46
Jacob <jacobbelay406@gmail.com>
2025-11-22 21:13:26 -0500

    basic stuff again

--- js/index.js
+++ js/index.js
@@ -14,3 +14,19 @@ function deleteDiary(){
 function newEntry(){
 
 }
+
+function previousPage(){
+
+}
+
+function darkMode(){
+
+}
+
+function actuallyDeleting(){
+
+}
+
+function callDiary(){
+    
+}
